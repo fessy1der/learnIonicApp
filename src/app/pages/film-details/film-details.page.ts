df53a8dcd0618506fd77e6aec0f9c70e16559068
Festus obi <fessy_wonder@yahoo.co.uk>
2021-05-14 15:20:48 +0200

    added email ionic native device feature

--- src/app/pages/film-details/film-details.page.ts
+++ src/app/pages/film-details/film-details.page.ts
@@ -1,3 +1,4 @@
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 import { ApiService } from './../../services/api.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
@@ -8,15 +9,30 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './film-details.page.html',
   styleUrls: ['./film-details.page.scss'],
 })
+
 export class FilmDetailsPage implements OnInit {
   film: any;
 
-  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private apiService: ApiService) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient,
+    private apiService: ApiService,
+    private emailComposer: EmailComposer) { }
 
   ngOnInit() {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
     this.apiService.getFilm(id).subscribe( res => {
       this.film = res;
     });
+  }
+
+  shareFilm() {
+    let email = {
+      to: 'saimon@devdactic.com',
+      subject: 'I love this one: ' + this.film.title,
+      body: 'Can you remember the opening?<br><br>\"' + this.film.opening_crawl + '\"',
+      isHtml: true
+    };
+    this.emailComposer.open(email);
   }
 }
