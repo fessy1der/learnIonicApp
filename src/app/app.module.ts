fad6808d4522d6dcf5caf2ca0fbaa598bd148e0b
Festus obi <fessy_wonder@yahoo.co.uk>
2021-05-14 18:31:31 +0200

    added ionic storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { EmailComposer } from '@ionic-native/email-composer/ngx';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   EmailComposer
 ],
